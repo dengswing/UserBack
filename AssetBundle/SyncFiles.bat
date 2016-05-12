b0cc309581e2803b69dffe8d0b0f10ca61ff7ef5
dengshiwei <dengsw@shinezone.com>
2016-05-12 18:24:29 +0800

    sync files bat

---
+++ AssetBundle/SyncFiles.bat
@@ -0,0 +1,2 @@
+xcopy AssetBundle\Assets\* ..\..\ShinezoneRoot\AssetBundle\AssetBundle\Assets\ /e /i /y
+xcopy AssetBundle\ProjectSettings\* ..\..\ShinezoneRoot\AssetBundle\AssetBundle\ProjectSettings\ /e /i /y
