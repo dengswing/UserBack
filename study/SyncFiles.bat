4310e79ceb87b5941196a19d0fe4e9fa69bab6a2
dengshiwei <dengsw@shinezone.com>
2016-05-17 09:52:26 +0800

    luaFrameworkUgui sync files

---
+++ study/SyncFiles.bat
@@ -0,0 +1,2 @@
+xcopy luaFrameworkUgui\Assets\* ..\..\ShinezoneRoot\luaFrameworkUgui\Assets\ /e /i /y
+xcopy luaFrameworkUgui\ProjectSettings\* ..\..\ShinezoneRoot\luaFrameworkUgui\ProjectSettings\ /e /i /y
