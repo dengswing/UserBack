4df3373a80f4a390dd0c5a4113a5c34b883d20a4
dengshiwei <dengsw@shinezone.com>
2016-05-24 11:21:38 +0800

    reporter demo

---
+++ Log/Reporter/Assets/scripts/Demo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Demo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("普通提示");
+        Debug.LogWarning("警告提示");
+        Debug.LogError("错误提示");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
