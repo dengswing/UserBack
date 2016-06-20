ff4abe52929c8b5b90f0dc5319595a54e17d8b9b
dengshiwei <dengsw@shinezone.com>
2016-06-20 14:21:11 +0800

    network demo

---
+++ Network_June/Assets/scripts/Network.cs
@@ -0,0 +1,111 @@
+﻿using System.Threading;
+
+namespace com.shinezone.network
+{
+    /// <summary>
+    /// 网络请求父类
+    /// </summary>
+    public class Network
+    {
+        public static readonly Network Instance = new Network();
+
+        private Thread sendThread;
+        private Thread recvThread;
+        static readonly object sendLockObj = new object();
+        static readonly object recvLockObj = new object();
+
+        public Network()
+        {
+            Init();
+        }
+
+        /// <summary>
+        /// 初始化，启动
+        /// </summary>
+        public void Initialise()
+        {
+            Start();
+        }
+
+        /// <summary>
+        /// 停止工作
+        /// </summary>
+        public void Shutdown()
+        {
+            sendThread.Abort();
+            recvThread.Abort();
+        }
+
+        #region 子类必须重载
+        /// <summary>
+        /// 发送消息，子类必须重载
+        /// </summary>
+        public virtual void Send()
+        {
+
+        }
+
+        /// <summary>
+        /// 接收信息，子类必须重载
+        /// </summary>
+        public virtual void Recv()
+        {
+
+        }
+        #endregion
+
+        private void Start()
+        {
+            sendThread.Start();
+            recvThread.Start();
+        }
+
+        private void Init()
+        {
+            sendThread = new Thread(SendHandler);
+            recvThread = new Thread(RecvHandler);
+        }
+
+        #region 线程处理
+
+        private void SendHandler()
+        {
+            while (true)
+            {
+                lock (sendLockObj)
+                {
+                    try
+                    {
+                        Send();
+                    }
+                    catch (System.Exception ex)
+                    {
+                        UnityEngine.Debug.LogError("SendHandler:" + ex.Message);
+                    }
+                }
+                Thread.Sleep(1);
+            }
+        }
+
+        private void RecvHandler()
+        {
+            while (true)
+            {
+                lock (recvLockObj)
+                {
+                    try
+                    {
+                        Recv();
+                    }
+                    catch (System.Exception ex)
+                    {
+                        UnityEngine.Debug.LogError("RecvHandler:" + ex.Message);
+                    }
+                }
+                Thread.Sleep(1);
+            }
+        }
+
+        #endregion
+    }
+}
